bcf9e4e3df3bf50fbb91abe7544a034a395a4033
Effie Song <yunfeisong1015@gmail.com>
2020-04-16 22:44:36 -0400

    Project_1
    Project1 beta

---
+++ Project _1/README.md
@@ -0,0 +1,36 @@
+# Project 1
+## Concept
+I want to tell a short story which is from a song. It is a very lovely and romantic story about how a chameleons find its own color. I was touched when I fisrt heard it. 
+
+Here's the story:
+Once upon a time, there was a chameleon. One day, it lay on the apple thinking. The apple is red, so becomes red. It rested on the elephant's back. The elephant is grey, so it becomes grey. 
+![image](https://github.com/EffieSong/openframeworks/raw/master/Project_1/Scene1.png)
+Wherever it goes, it has to keep changing colors. It felt very unhappy. It didn't know what its own color was. 
+![image](https://github.com/EffieSong/openframeworks/raw/master/Project_1/Scene1.png)
+So it found a leaf it liked and turned green with it. It was happy all spring and summer. It thought it has its own color. However, by autumn, the leaves were turning yellow. It turned yellow with the leaves, and then, in winter, it fell from the tree with the leaves. It fell to the ground, being so sad that it did not know what to do. 
+![image](https://github.com/EffieSong/openframeworks/raw/master/Project_1/Scene1.png)
+At this time, it met another chameleon. This chameleon had the same trouble. They all had to keep changing colors. However, the other chameleon was more clever. It told it that they could always walk together, so that wherever they went, they would still turn red when they  meet apples, but not because of the apples, just because they became the color of each other.
+
+
+I think people will have different feelings about this story. For me, it reflects something in a love relationship, which is that people should be with people who are similar to them. Two people whose independent personalities are not so strong will be happy being together. But if one's personality is too strong then the other, the other one will become an accessory. I hope everyone can find the right person for themselves, and more importantly, find themselves.
+
+At first I thought about adding some possible interaction, such as exporing the environment with mouse. But later I thought it was not necessary, so I decided to make it a simple linear story. However if you have any suggestion about adding interaction please add coments below youtube video !
+
+## Future iterations
+* Add more scene
+* Improve the graphic
+* Add background music
+* Add narrator
+* Any interactions?
+
+## Process quesionts
+*  Import SVG to OF and manipulate the color of the image.
+*  I'm also thinking about the meaning of design motion graphic by coding rather than After Effects.
+
+## Evaluation
+I improve my structure of coding through the process. However I find it hard to implement some animation only using OF. 
+## Link for video
+[![](https://github.com/EffieSong/openframeworks/raw/master/Project_1/Scene4.png)](https://youtu.be/9X6yWSH70ko)
+
+
+
---
+++ Project _1/src/ofApp.cpp
@@ -0,0 +1,692 @@
+#include "ofApp.h"
+ofSoundPlayer happySound;
+ofSoundPlayer sadSound;
+ofSoundPlayer surpriseSound;
+ofSoundPlayer angrySound;
+Chameleon chameleon;
+Element redApple(0,0);
+Element grayElephant(0,0);
+Element Leaf(743,424);
+Element backTree(1400,0);
+Element bigTree(1400,0);
+Element flower(1400,0);
+Element foregroundFlower(1400,0);
+Element branch(0,0);
+ofColor background(254,253,239);
+ofColor chamColor(107,147,103);
+ofColor ground(179,121,76);
+ofColor appleColor(239,62,54);
+ofColor leafColor(40,150,121);
+float sad = 0;
+int emotion = 1;
+int alpha = 255;
+ofFbo fbo_fadeIn;
+ofFbo fbo_fadeOut;
+ofImage apple;
+ofImage elephant;
+ofImage scene6BG;
+ofImage branchImg;
+ofImage leaf;
+ofImage backTreeImg;
+ofImage bigTreeImg;
+ofImage flowerImg;
+ofImage foregroundFlowerImg;
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetBackgroundColor(background);
+    ofSetFrameRate(60);
+    ofSetCircleResolution(50);
+    ofSetPolyMode(OF_POLY_WINDING_POSITIVE);
+    
+    //load sound
+    happySound.load("happy.wav");
+    sadSound.load("sad.wav");
+    surpriseSound.load("surprise.wav");
+    angrySound.load("angry.wav");
+    
+    //load picture
+    apple.load("apple200.png");
+    elephant.load("elephant.png");
+    scene6BG.load("scene6BG.jpg");
+    leaf.load("leaf.png");
+    foregroundFlowerImg.load("foregroundFlower.png");
+    flowerImg.load("flower.png");
+    bigTreeImg.load("bigTree.png");
+    backTreeImg.load("backTree.png");
+    branchImg.load("branch.png");
+    
+    //load font
+    font.load("Montserrat-Medium.ttf",20);
+  
+    emotionButton.addListener(this, &ofApp::emotionButtonPressed);
+   
+    gui.setup();
+    gui.add(volume.setup("volume",1.0,0.0,1.0));
+    gui.add(emotionButton.setup("emotion"));
+    gui.add(guiPos.setup("position",{ofGetWidth()/2,ofGetHeight()/2},{-500,-500},{ofGetWidth(),ofGetWidth()}));
+    gui.add(guiColor.setup("color", ofColor(168,195,120), ofColor(0, 0), ofColor(255, 255)));
+    gui.add(sadDegree.setup("sadDegree",10,0,10));
+    gui.add(screenSize.setup("screen size", ofToString(ofGetWidth())+"x"+ofToString(ofGetHeight())));
+    
+    fbo_fadeOut.allocate(ofGetWidth(), ofGetHeight(),GL_RGBA32F);
+    fbo_fadeOut.begin();
+     ofClear(255, 255, 255,0);
+    fbo_fadeOut.end();
+    fbo_fadeIn.allocate(ofGetWidth(), ofGetHeight(),GL_RGBA32F);
+    fbo_fadeIn.begin();
+        ofClear(255, 255, 255,0);
+    fbo_fadeIn.end();
+    
+    chameleon.setCurrentPos(350, 550);
+    chameleon.color = chamColor;
+    redApple.color = appleColor;
+    redApple.r = 100;
+    Leaf.r = 100;
+    Leaf.color = leafColor;
+    foregroundFlower.r = 100;
+    flower.r = 80;
+    bigTree.r = 80;
+
+
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+     happySound.setVolume(volume);
+     sadSound.setVolume(volume);
+     surpriseSound.setVolume(volume);
+     angrySound.setVolume(volume);
+   
+    
+    
+// if(redApple.pos.x<490) redApple.stopRotate();
+
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    if(ofGetElapsedTimef()<30) drawScene3(0);
+    if(ofGetElapsedTimef()>30 && ofGetElapsedTimef()<60) drawScene2(30);
+    if(ofGetElapsedTimef()>60 && ofGetElapsedTimef()<109) drawScene3(60);
+    //drawfadeIn();
+    
+    
+    gui.draw();
+    timeString = "elapsed time: "+ofToString(ofGetElapsedTimef());
+    ofSetColor(10,12,10);
+    font.drawString(timeString, 200,98);
+    font.drawString(eventString, 200,150);
+    
+ 
+   
+    
+}
+
+
+void ofApp::drawScene1(float time){
+   // ofSetColor(ground);
+    if(ofGetElapsedTimef()<1+time){
+        chameleon.setCurrentPos(507, 362);
+        redApple.pos = ofVec3f(ofGetWidth()/2 , 427);
+        grayElephant.pos = ofVec3f(ofGetWidth()/2,ofGetHeight()+elephant.getHeight()/2);
+    }
+    if(ofGetElapsedTimef()>6+time){
+        chameleon.changeColorByElement(redApple, 0.02);//change color
+    }
+    if(ofGetElapsedTimef()>10+time && ofGetElapsedTimef()<18+time){//apple away
+        chameleon.moveTo(ofVec3f(507,ofGetHeight()+elephant.getHeight()/2-530+285),1);
+        redApple.rZ -= 1;
+        redApple.pos -= ofVec3f(3,0);
+    }
+    if(ofGetElapsedTimef()>18+time){ //elephant appear
+        chameleon.moveTo(ofVec3f(507,285),0.6);
+        grayElephant.moveTo(ofVec3f(520,530), 0.6);
+       // grayElephant.interpolateByPct(ofGetWidth()/2,ofGetHeight()+elephant.getHeight()/2,520,512,0.002f);
+        ofSetColor(255);
+         ofPushMatrix();
+            ofTranslate(grayElephant.pos);
+            ofScale(1.5, 1.5);
+        elephant.draw(0-elephant.getWidth()/2,0-elephant.getHeight()/2);
+        ofPopMatrix();
+      }
+    if(ofGetElapsedTimef()>23+time){
+        chameleon.changeColor(ofColor(138,137,143), 0.05);
+    }
+     
+    if(ofGetElapsedTimef()>5+time){ //draw apple
+        ofPushMatrix();
+        ofTranslate(redApple.pos);
+            ofScale(1.7, 1.7);
+            ofRotateZDeg(redApple.rZ);
+            ofSetColor(255);
+            apple.draw(0 - apple.getWidth()/2, 0 - apple.getHeight()/2);  // draw at new 0,0
+        ofPopMatrix();
+    }
+    chameleon.render(emotion);
+    drawfadeIn();
+    if(ofGetElapsedTimef()>29+time){
+         drawfadeOut();
+    }
+}
+
+
+
+
+float speedBackTree;
+float speedFrontFlower;
+float speedBigTree;
+float speedflower;
+
+void ofApp::drawScene2(float time){
+    chameleon.setCurrentPos(314, 527);
+    chameleon.walk(0.5);
+    speedBackTree = 0.0004;
+    speedBigTree = 0.0008;
+    speedflower = 0.0009;
+    speedFrontFlower = 0.003;
+        backTree.interpolateByPct(1100, 96, -300, 96, speedBackTree);
+        bigTree.interpolateByPct(1000, 88, -300, 88, speedBigTree);
+        
+     if(ofGetElapsedTimef()> 3.000+time){
+         foregroundFlower.interpolateByPct(1400, 668, -300, 668, speedFrontFlower);
+     };
+    if(ofGetElapsedTimef()> 5.000+time){
+           flower.interpolateByPct(1400, 531, -300, 531, speedflower);
+        };
+    if(ofGetElapsedTimef()>8.000+time){
+        chameleon.changeColor(ofColor(149,87,76), 0.05);
+    }
+    if(ofGetElapsedTimef()>15.000+time){
+        chameleon.changeColor(ofColor(198,92,127), 0.06);
+    }
+    
+    ofSetColor(34,79,91);
+    ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());//bg
+    ofSetColor(255);
+    backTreeImg.draw(backTree.pos.x-backTreeImg.getWidth()/2,backTree.pos.y-backTreeImg.getHeight()/2);
+    ofSetColor(247,178,77);
+    ofDrawRectangle(0, 445, ofGetWidth(), ofGetHeight());//ground
+      
+    ofSetColor(255);
+    bigTreeImg.draw(bigTree.pos.x-bigTreeImg.getWidth()/2,bigTree.pos.y-bigTreeImg.getHeight()/2);
+    chameleon.render(emotion);
+    ofSetColor(255);
+    flowerImg.draw(flower.pos.x-flowerImg.getWidth()/2,flower.pos.y-flowerImg.getHeight()/2);
+    foregroundFlowerImg.draw(foregroundFlower.pos.x-foregroundFlowerImg.getWidth()/2,foregroundFlower.pos.y-foregroundFlowerImg.getHeight()/2);
+    
+    if(ofGetElapsedTimef()> 20.000+time){
+        emotion = 2;
+        sad += 0.04;
+        if(sad >10)sad=10;
+    };
+    drawfadeIn();
+    if(ofGetElapsedTimef()>29.000+time){
+        drawfadeOut();
+    }
+}
+
+ofVec3f branchPos(-32,376);
+//float rZ=0;
+float groundY=1000;
+ofVec3f ChamScene6Pos(350,450);
+
+void ofApp::drawScene3(float time){
+    ofSetColor(216,226,186);
+    ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());//bg
+    ofSetColor(255);
+    branchImg.draw(branchPos.x,branchPos.y);
+    
+    if(ofGetElapsedTimef()< 5.000+time)
+    {
+        chameleon.setCurrentPos(320, 470);
+        emotion = 2;
+        sad = 5;
+    }
+    if(ofGetElapsedTimef()> 5.000+time && ofGetElapsedTimef()<15+time){
+        chameleon.moveTo(Leaf.pos-ofVec3f(20,5), 1.0);
+        chameleon.changeColorByElement(Leaf,0.06);
+    }
+    if(ofGetElapsedTimef()>10.000+time && ofGetElapsedTimef()<15+time){
+        sad -= 0.04;
+        if(sad <0)sad=0;
+    }
+    
+    if(ofGetElapsedTimef()>20+time){
+       chameleon.changeColor(ofColor(225,180,29),0.04);
+    }
+    if(ofGetElapsedTimef()> 25.000+time){
+        Leaf.rotate(0.2);
+        branchPos += ofVec3f(0,-1,0);
+        groundY -= 1;
+        if(groundY < 350){
+            groundY = 350;
+        }
+    }
+    if(ofGetElapsedTimef()> 33.000+time){
+        emotion = 2;
+        sad += 0.04;
+        if(sad >10)sad=10;
+    };
+    //display
+    ofSetColor(104, 135, 125);
+    ofDrawRectangle(0, groundY, ofGetWidth(), ofGetHeight());
+    ofPushMatrix();
+        ofTranslate(Leaf.pos.x-leaf.getWidth()/2,Leaf.pos.y-leaf.getHeight()/2);
+        ofRotateZDeg(Leaf.rZ);
+        ofSetColor(255);
+        leaf.draw(0,0);
+    ofPopMatrix();
+    chameleon.render(emotion);
+    drawfadeIn();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+   eventString = "mouseMoved = (" + ofToString(x) + ", " + ofToString(y) + ")";
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+void ofApp::emotionButtonPressed(){
+    emotion = emotion+1;
+    if( emotion>4){
+        emotion = 1;
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+
+void ofApp::drawfadeIn(){
+    fbo_fadeIn.begin();
+    ofSetRectMode(OF_RECTMODE_CORNER);
+    ofSetColor(0, 0, 0,alpha);
+    ofDrawRectangle(0,0,ofGetWidth(),ofGetHeight());
+    //  ofSetColor(255, 230, 200,alpha);
+    //  font.drawString(text, ofGetWidth()/2-font.stringWidth(text)/2, ofGetHeight()/2);
+    fbo_fadeIn.end();
+    fbo_fadeIn.draw(0,0);
+    alpha -= 2;
+    if (alpha<0)fbo_fadeIn.clear();
+}
+
+int alphaFadeOut =0;
+void ofApp::drawfadeOut(){
+    fbo_fadeOut.begin();
+    ofSetRectMode(OF_RECTMODE_CORNER);
+    ofSetColor(0, 0, 0,alphaFadeOut);
+    ofDrawRectangle(0,0,ofGetWidth(),ofGetHeight());
+    //  ofSetColor(255, 230, 200,alpha);
+    //  font.drawString(text, ofGetWidth()/2-font.stringWidth(text)/2, ofGetHeight()/2);
+    fbo_fadeOut.end();
+    fbo_fadeOut.draw(0,0);
+    alphaFadeOut += 2;
+    if (alphaFadeOut>255)fbo_fadeOut.clear();
+}
+
+void Chameleon:: walk(float step){
+    if(foot_back < -7.0 || foot_back >2.0 )dr *= -1;
+    foot_back -= step*dr;
+    foot_front += step*dr;
+}
+
+
+
+void Chameleon::changeColorByElement(Element object,float a){
+    float dis = pos.distance(object.pos);
+    if (dis < r+object.r){
+        color.lerp(object.color,a);
+    }
+    //cout<< dis << endl;
+}
+
+void Chameleon::changeColor(ofColor targetColor,float a){
+    color.lerp(targetColor,a);
+};
+
+//Element
+void Chameleon::setCurrentPos(int x, int y){
+    pos.x = x;
+    pos.y = y;
+}
+void Chameleon::setTargetPos(int x, int y){
+    targetPos.x = x;
+    targetPos.y = y;
+}
+//
+void Chameleon::zenoToPoint(int x, int y,float catchUpSpeed){
+    pos.x = catchUpSpeed * x + (1-catchUpSpeed) * pos.x;
+    pos.y = catchUpSpeed * y + (1-catchUpSpeed) * pos.y;
+}
+
+void Chameleon::moveTo(ofVec3f target, float scale){
+    ofVec3f speed = target-pos;
+    speed.getNormalized();
+    speed.scale(scale);
+    pos += speed;
+}
+
+void Chameleon::render(int emotion){
+    ofSetColor(color);
+    ofPushMatrix();
+    ofTranslate(pos.x, pos.y);
+//body
+        ofBeginShape();
+        for (int i = 0; i < 20; i++){
+            float anglef = ((float)i / 19.0f) * PI;
+            float x =  120 * cos(anglef);
+            float y = - 110 * sin(anglef);
+            ofVertex(x,y);
+        }
+        ofEndShape();
+    
+//tail
+        ofBeginShape();
+           for (int i = 0; i < 20; i++){
+            float anglef = ((float)i / 19.0f) * PI/2;
+            float x =  -20-100 * cos(anglef);
+            float y =  100 * sin(anglef);
+            ofVertex(x,y);
+        }
+           for (int i = 0; i < 20; i++){
+        
+            float anglef = ((float)i / 19.0f) * PI-PI/2;
+            float x =  -20+40 * cos(anglef);
+            float y =  60-40 * sin(anglef);
+            ofVertex(x,y);
+        }
+           for (int i = 0; i < 20; i++){
+        
+            float anglef = ((float)i / 19.0f) * PI-PI/2;
+            float x =  -20+20 * cos(anglef);
+            float y =  40+20 * sin(anglef);
+            ofVertex(x,y);
+        }
+           for (int i = 0; i < 20; i++){
+        
+            float anglef = ((float)i / 19.0f) * PI-PI/2;
+            float x =  -20-35 * cos(anglef);
+            float y =  25-35 * sin(anglef);
+            ofVertex(x,y);
+        }
+         ofEndShape();
+//foot
+    
+    ofBeginShape();
+        ofCurveVertex(-15,0);
+        ofCurveVertex(0,0);
+        ofCurveVertex(foot_back,10);//
+        ofCurveVertex(-15,0);
+        ofCurveVertex(0,0);
+        ofCurveVertex(5,10);
+    ofEndShape();
+    ofBeginShape();
+        ofCurveVertex(15,0);
+        ofCurveVertex(30,0);
+        ofCurveVertex(foot_front,10);//
+        ofCurveVertex(15,0);
+        ofCurveVertex(30,0);
+        ofCurveVertex(35,10);
+    ofEndShape();
+
+//head
+
+    ofTranslate(80,0);
+    ofBeginShape();
+        ofCurveVertex(0,0);//4
+        ofCurveVertex(0,-120);//top 1
+        ofCurveVertex(50,-96);//2
+        ofCurveVertex(98,-25);//mouth 3
+        ofCurveVertex(0,0);//4
+        ofCurveVertex(0,-120);//1
+        ofCurveVertex(50,-96);//2
+    ofEndShape();
+ 
+    switch (emotion) {
+    default :
+     happySound.play();
+   // eyes
+        ofSetColor(255, 255, 255);
+        ofDrawCircle(29, -65, 30);
+        ofSetColor(10,10,10);
+        ofDrawCircle(34, -65, 6);
+    //mouth
+        ofSetColor(10, 10, 10);
+        ofSetLineWidth(3);
+        ofDrawLine(90,-20,80, -18);
+        ofDrawLine(80, -18 ,60,-23);
+    
+     case 1:    //happy/////////
+    //   happySound.play();
+    //eyes
+       ofSetColor(255, 255, 255);
+       ofDrawCircle(29, -65, 30);
+       ofSetColor(10,10,10);
+       ofDrawCircle(34, -65, 6);
+    //mouth
+       ofSetColor(10, 10, 10);
+       ofSetLineWidth(3);
+       ofDrawLine(90,-25,75, -20);
+       ofDrawLine(75, -20,60,-23);
+            
+    break;
+
+   case 2://///sad
+     //  sadSound.play();
+ //eye
+       ofSetColor(255, 255, 255);
+       ofBeginShape();
+        for (int i = 0; i < 20; i++){
+       
+                 float anglef = ((float)i / 19.0f) * (2-ofMap(sad,0,10,0,0.9))*PI-PI*(0.7-ofMap(sad,0,10,0,0.45));
+                 float x =  29+30 * cos(anglef);
+                 float y =  -65+30 * sin(anglef);
+                 ofVertex(x,y);
+             }
+       ofEndShape();
+           ofSetColor(10,10,10);
+           ofDrawCircle(34+ofMap(sad,0,10,0,5), -65+ofMap(sad,0,10,0,14), 6);
+         //  ofDrawCircle(39, -51, 6);
+//mouth
+           ofSetColor(10, 10, 10);
+           ofSetLineWidth(3);
+           ofDrawLine(90,-25,75, -20-sad);
+           ofDrawLine(75, -20-sad,62,-23+ofMap(sad,0,10,0,5));
+    break;
+            
+    case 3:
+    //angry///////////////
+            angrySound.play();
+             //eye
+            ofSetColor(255, 255, 255);
+        ofBeginShape();
+              for (int i = 0; i < 20; i++){
+            
+                        float anglef = ((float)i / 19.0f) * 1.2*PI+PI*0.1;
+                        float x =  29+30 * cos(anglef);
+                        float y =  -65+30 * sin(anglef);
+                        ofVertex(x,y);
+                    }
+        ofEndShape();
+            ofSetColor(10,10,10);
+            ofDrawCircle(39, -60, 6);
+    //mouth
+            ofSetColor(10, 10, 10);
+            ofSetLineWidth(3);
+            ofDrawLine(90,-25,75, -36);
+            ofDrawLine(75, -36 ,62,-18);
+            break;
+    case 4:
+       //surprise///////////////
+            surpriseSound.play();
+        //eye
+           ofSetColor(255, 255, 255);
+           ofDrawCircle(29, -65, 30);
+           ofSetColor(10,10,10);
+           ofDrawCircle(34, -65, 6);
+    //mouth
+            ofSetColor(10, 10, 10);
+            ofNoFill();
+            ofSetLineWidth(3);
+            ofDrawEllipse(70,-25,20,20);
+            ofFill();
+    break;
+    };
+            
+
+        ofPopMatrix();
+        
+}
+
+//Element
+
+void Element::setTargetPos(int x, int y){
+    targetPos.x = x;
+    targetPos.y = y;
+}
+//
+void Element::zenoToPoint(float catchUpSpeed){
+    pos.x = catchUpSpeed * targetPos.x + (1-catchUpSpeed) * pos.x;
+    pos.y = catchUpSpeed * targetPos.y + (1-catchUpSpeed) * pos.y;
+}
+
+void Element::interpolateByPct(int x1, int y1, int x2, int y2, float speed){
+    pct += speed;        // increase by a certain amount
+    if (pct > 1) {
+        pct = 1;
+    }
+    pos.x = (1-pct) * x1 + (pct) * x2;
+    pos.y = (1-pct) * y1 + (pct) * y2;
+   // cout<<pct<<endl;
+}
+
+
+
+int centerX;
+int centerY;
+
+void Element::renderLeaf(){
+    centerX = pos.x;
+    centerY = pos.y;
+    ofSetColor(100,80, 50);
+    ofFill();
+    ofSetRectMode(OF_RECTMODE_CENTER);
+    ofDrawRectangle(pos+ofVec3f(0,130), 10, 120);
+    
+    ofSetColor(color);
+    ofFill();
+    ofBeginShape();
+    ofVertex(centerX,centerY-150);
+    ofVertex(centerX-70,centerY-50);
+    ofVertex(centerX-150,centerY-80);
+    ofVertex(centerX-150,centerY);
+    ofVertex(centerX-70,centerY+50);
+    ofVertex(centerX-110,centerY+100);
+    ofVertex(centerX-70,centerY+130);
+    ofVertex(centerX,centerY+80);
+     ofVertex(centerX+70,centerY+130);
+     ofVertex(centerX+110,centerY+100);
+    ofVertex(centerX+70,centerY+50);
+    ofVertex(centerX+150,centerY);
+    ofVertex(centerX+150,centerY-80);
+    ofVertex(centerX+70,centerY-50);
+    ofEndShape(true);
+}
+
+void Element::renderApple(){
+    ofPushMatrix();
+    ofTranslate(pos.x, pos.y);
+    ofScale(1.5, 1.5);  // can be 3 dimensional
+       ofRotateZDeg(rZ);
+       ofSetColor(255);
+       apple.draw(0 - apple.getWidth()/2, 0 - apple.getHeight()/2);  // draw at new 0,0
+     ofPopMatrix();
+}
+
+void Element::appleMove(){
+    rZ -=5;
+    redApple.interpolateByPct(ofGetWidth(),450,520,450,0.005f);
+    if(pct == 1)rZ = 0;
+}
+void Element::appleStopMove(){
+    rZ = 0;
+}
+void Element::renderElephant(){
+    ofPushMatrix();
+    ofTranslate(pos.x, pos.y);
+    ofScale(1.8, 1.8);  // can be 3 dimensional
+       ofRotateZDeg(rZ);
+       ofSetColor(255);
+       elephant.draw(0 - elephant.getWidth()/2, 0 - elephant.getHeight()/2);  // draw at new 0,0
+     ofPopMatrix();
+}
+void Element::elephantMove(){
+  grayElephant.interpolateByPct(ofGetWidth(),250,520,250,0.002f);
+}
+
+int b = 1;
+void Element::rotate(float a){
+    rZ += a*b;
+    if(rZ > 10 || rZ<-10){
+        b *= -1;
+    }
+}
+void Element::stopRotate(){
+    rZ =0;
+}
+void Element::moveTo(ofVec3f target, float scale){
+    ofVec3f speed = target-pos;
+    speed.getNormalized();
+    speed.scale(scale);
+    pos += speed;
+}
+
+
